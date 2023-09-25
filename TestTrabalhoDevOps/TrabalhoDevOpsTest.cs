d35331aa5252688182f746e6494afe6ac06419e3
Pedro F R Novaes <pe.fer@hotmail.com>
2023-09-24 22:12:38 -0300

    Adicionando os testes

---
+++ TestTrabalhoDevOps/TrabalhoDevOpsTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using TrabalhoDevOps.Controllers;
+
+namespace TrabalhoDevOps.Tests
+{
+    public class WeatherForecastControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsFiveWeatherForecasts()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            var forecasts = Assert.IsType<WeatherForecast[]>(result);
+            Assert.Equal(5, forecasts.Length);
+        }
+
+        [Fact]
+        public void Get_ReturnsValidWeatherForecasts()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            var forecasts = Assert.IsType<WeatherForecast[]>(result);
+            Assert.Equal(5, forecasts.Length);
+            foreach (var forecast in forecasts)
+            {
+                Assert.InRange(forecast.TemperatureC, -20, 55); // Temperature range check
+                Assert.Contains(forecast.Summary, WeatherForecastController.Summaries); // Valid summary check
+            }
+        }
+    }
+}
